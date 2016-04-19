c648b156a0164f19778075cf6e4bc9c2e8bdff75
Geandré Gomes de Oliveira <geandre.oliveira@gmail.com>
2016-04-19 10:55:38 -0300

    Create package-info.java

---
+++ src/main/java/br/gov/serpro/infoconv/ws/cnpj/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "https://hom-infoconv.receita.fazenda.gov.br/ws/cnpj/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.gov.serpro.infoconv.ws.cnpj;
